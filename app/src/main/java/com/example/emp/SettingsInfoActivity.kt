396f2b4422b2ccecd066380edafe189adca6b149
jasa <jasa.kertpust@gmail.com>
2025-01-05 00:43:16 +0100

    dodal settingsininfo deluje tudi delete

--- app/src/main/java/com/example/emp/SettingsInfoActivity.kt
+++ app/src/main/java/com/example/emp/SettingsInfoActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsInfoActivity : AppCompatActivity() {
-
+    private val sharedPreferences by lazy { getSharedPreferences("Palettes", MODE_PRIVATE) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings_info)
@@ -16,6 +16,7 @@ class SettingsInfoActivity : AppCompatActivity() {
         deleteAllDataButton.setOnClickListener {
             // Handle the delete all data action
             // For now, show a simple toast message
+            sharedPreferences.edit().clear().apply()
             Toast.makeText(this, "All data deleted!", Toast.LENGTH_SHORT).show()
         }
     }
